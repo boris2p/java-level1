a1160d9a1ec919ea61d1bb8a3bbda74c62a2de97
boris2p <boris2p@mail.ru>
2025-03-07 21:41:42 +0300

    Создание классов в соответствии с материалами модуля lesson4

---
+++ lesson4/src/ru/b2p/lesson4/ifElseExample.java
@@ -0,0 +1,22 @@
+package ru.b2p.lesson4;
+
+public class ifElseExample {
+
+    public static void main(String[] args) {
+        int value = 10;
+
+        if (value > 0) {
+            System.out.println("Число является положительным");
+        } else if (value < 0) {
+            System.out.println("Число является отрицательным");
+        } else if (value < 1000) {
+            System.out.println("Число является отрицательным");
+        } else if (value < 0) {
+            System.out.println("Число является отрицательным");
+        } else {
+            System.out.println("Число является нулем");
+        }
+
+
+    }
+}
